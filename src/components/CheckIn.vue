901ab0b7a4305c3efc0cb985072b1b4cefe40358
arihh <arihh0615@gmail.com>
2019-01-15 16:52:31 +0900

    post通信してデータを記録する

--- src/components/CheckIn.vue
+++ src/components/CheckIn.vue
@@ -6,15 +6,76 @@
       <label>経度</label>
       <input type="text" v-model="lon">
     </div>
-    <input type="text" v-model="comment">
-    <button class="checkin btn btn-primary">チェックイン!</button>
+    <div>
+      <label>コメント</label>
+      <input type="text" v-model="comment">
+    </div>
+    <button @click="checkIn" class="checkin btn btn-primary">チェックイン!</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "CheckIn",
-  methods: {}
+  data: function() {
+    return {
+      lat: "",
+      lon: "",
+      comment: ""
+    };
+  },
+  methods: {
+    checkIn: function(e) {
+      if (!navigator.geolocation) {
+        alert("位置情報が取れません");
+        return;
+      }
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          console.log(
+            "取得:" + position.coords.latitude + "," + position.coords.longitude
+          );
+          this.lat = position.coords.latitude;
+          this.lon = position.coords.longitude;
+
+          var qs = require("qs");
+          axios
+            .post(
+              "http://localhost:3000/place_records.json",
+              qs.stringify({
+                place_record: {
+                  lat: this.lat,
+                  lon: this.lon,
+                  comment: this.comment
+                }
+              }),
+              {
+                headers: { "Content-Type": "application/x-www-form-urlencoded" }
+              }
+            )
+            .then(res => {
+              //リクエスト成功時の処理
+            });
+        },
+        error => {
+          switch (error.code) {
+            case 1: //PERMISSION_DENIED
+              alert("位置情報の利用が許可されていません");
+              break;
+            case 2: //POSITION_UNAVAILABLE
+              alert("現在位置が取得できませんでした");
+              break;
+            case 3: //TIMEOUT
+              alert("タイムアウトになりました");
+              break;
+            default:
+              alert("その他のエラー(エラーコード:" + error.code + ")");
+              break;
+          }
+        }
+      );
+    }
+  }
 };
 </script>
 
